4f5c157ea82724c3ee30e211d098bf5a3f3a30cc
SamZhang123 <31572111+SamZhang123@users.noreply.github.com>
2019-10-22 12:43:28 -0500

    Add files via upload

--- src/DogFood.java
+++ src/DogFood.java
@@ -18,18 +18,134 @@ public class DogFood
    {
 		Scanner chopper = new Scanner(line);
 		double cups = 0;
-
-		//calc code goes here
-
+                int space = 0;
+                for(int i = 0; i<line.length();i++)
+                {
+                    if(line.charAt(i)==' '&& space ==0)
+                    {
+                        String x = line.substring(0, i);
+                        space=i;
+                        int weight = Integer.parseInt(x);
+                        if(weight>=2 && weight<=4)
+                        {
+                            cups += 0.5;
+                        }
+                        else if(weight>=5 && weight<=7)
+                        {
+                            cups += 1.0;
+                        }
+                        else if(weight>=8 && weight<=9)
+                        {
+                            cups += 1.5;
+                        }
+                        else if(weight>=10 && weight<=19)
+                        {
+                            cups += 2.0;
+                        }
+                        else if(weight>=20 && weight<=39)
+                        {
+                            cups += 3.5;
+                        }
+                        else if(weight>=40 && weight<=59)
+                        {
+                            cups += 4.5;
+                        }
+                        else if(weight>=60 && weight<=79)
+                        {
+                            cups += 6.0;
+                        }
+                        else if(weight>=80)
+                        {
+                            cups += 7.5;
+                        }
+                    }
+                    else if(line.charAt(i)==' '&& space!=0)
+                    {
+                        String x = line.substring(space+1, i);
+                        space=i;
+                        int weight = Integer.parseInt(x);
+                        if(weight>=2 && weight<=4)
+                        {
+                            cups += 0.5;
+                        }
+                        else if(weight>=5 && weight<=7)
+                        {
+                            cups += 1.0;
+                        }
+                        else if(weight>=8 && weight<=9)
+                        {
+                            cups += 1.5;
+                        }
+                        else if(weight>=10 && weight<=19)
+                        {
+                            cups += 2.0;
+                        }
+                        else if(weight>=20 && weight<=39)
+                        {
+                            cups += 3.5;
+                        }
+                        else if(weight>=40 && weight<=59)
+                        {
+                            cups += 4.5;
+                        }
+                        else if(weight>=60 && weight<=79)
+                        {
+                            cups += 6.0;
+                        }
+                        else if(weight>=80)
+                        {
+                            cups += 7.5;
+                        }
+                    }
+                }
+                if(line.charAt(line.length()-1)!=' ')
+                {
+                    String x = line.substring(space+1, line.length());
+                    int weight = Integer.parseInt(x);
+                        if(weight>=2 && weight<=4)
+                        {
+                            cups += 0.5;
+                        }
+                        else if(weight>=5 && weight<=7)
+                        {
+                            cups += 1.0;
+                        }
+                        else if(weight>=8 && weight<=9)
+                        {
+                            cups += 1.5;
+                        }
+                        else if(weight>=10 && weight<=19)
+                        {
+                            cups += 2.0;
+                        }
+                        else if(weight>=20 && weight<=39)
+                        {
+                            cups += 3.5;
+                        }
+                        else if(weight>=40 && weight<=59)
+                        {
+                            cups += 4.5;
+                        }
+                        else if(weight>=60 && weight<=79)
+                        {
+                            cups += 6.0;
+                        }
+                        else if(weight>=80)
+                        {
+                            cups += 7.5;
+                        }
+                }
+                System.out.println("Total cups: "+cups);
+                amount = getAmount(cups);
    }
 
-   public double getAmount()
+   public double getAmount(double cups)
    {
-   	return 0;
+   	return Math.ceil(cups/60);
    }
 
 	public String toString()
 	{
-		return "";
+		return "Amount of 10-pounds bags: "+amount;
 	}
 }
--- src/DogFoodRunner.java
+++ src/DogFoodRunner.java
@@ -3,12 +3,18 @@
 //Name -
 
 import java.util.Scanner;
-import static java.lang.System.*;
-import java.lang.Math;
+import java.io.*;
 
 public class DogFoodRunner
 {
-	public static void main(String[] args)
+	public static void main(String[] args)throws IOException
 	{
+            Scanner in = new Scanner(new File("DogFood.txt"));
+            DogFood x;
+            while(in.hasNextLine())
+            {
+                x = new DogFood(in.nextLine());
+                System.out.println(x.toString());
+            }
 	}
 }
